9c317614572b7b6acc23472be29cc0dc098e8309
Jake Landrum <jakelandrum2004@gmail.com>
2025-04-03 08:30:48 -0500

    routerRoutes

--- src/Router/index.js
+++ src/Router/index.js
@@ -1,14 +1,18 @@
 import { createRouter, createWebHashHistory  } from 'vue-router'
-import Home from '../components/home.vue'
-//import About from '../pages/aboutPage.vue'
-//import Projects from '../pages/projectsPage.vue'
+import Home from '../views/home.vue'
+import Wordle from '../views/wordle.vue'
+import RedButton from '../views/redbutton.vue'
+import Captcha from '../views/captcha.vue'
+import FinalPuzzle from '../views/finalpuzzle.vue'
 
 const router = createRouter({
     history: createWebHashHistory('/escaperoom/'),
     routes: [
-        { path: '/', name: "Home", component: Home }
-        //{ path: '/about', name: "About", component: About },
-        //{ path: '/projects', name: "Projects", component: Projects }
+        { path: '/', name: "Home", component: Home },
+        { path: '/wordle', name: "Wordle", component: Wordle },
+        { path: '/redbutton', name: "Red Button", component: RedButton },
+        { path: '/captcha', name: "Home", component: Captcha },
+        { path: '/final', name: "Home", component: FinalPuzzle }
     ]
 })
 
